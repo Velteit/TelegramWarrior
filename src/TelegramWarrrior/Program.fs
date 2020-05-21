a8b0d575018cf71073220e983f9ca3ec8272dd91
Black Cat <ktekuchev@gmail.com>
2020-05-22 00:42:44 +0200

    [Chore] regex win

--- src/TelegramWarrrior/Program.fs
+++ src/TelegramWarrrior/Program.fs
@@ -3,13 +3,31 @@
 module TelegramWarrrior.EntryPoint
 
 open System
-open Funogram.Bot
 open Funogram.Api
+open Funogram.Types
+open Funogram.Telegram.Bot
+open Funogram.Telegram.Api
+open Funogram.Telegram.Types
 open Thoth.Json.Net
 open FSharpPlus
 open TelegramWarrrior.Utils
 open TelegramWarrrior.Model
+open System.Text.RegularExpressions
 
+
+let getTasks () =
+  let mutable tasks : Task list = []
+  let mutable hasResult: bool = true
+  Process.run
+    "task"
+    ["export"]
+    [(fun _ args ->
+        // printfn "%s" args.Data
+        if (not << String.IsNullOrEmpty) args.Data then Decode.fromString Task.Decoder args.Data |> (function | Ok(task) -> tasks <- task::tasks | Error(e) -> printf "%s" e))]
+    (fun ex ->
+        // printfn "%s" ex.Message
+        hasResult <- false)
+  if hasResult then Some(tasks) else None
 
 let onHello context =
   monad' {
@@ -44,10 +62,17 @@ let onTask context =
   }
   |> ignore
 
-let onTaskList (tasks: Task list) context =
+let onTaskList (arguments: (string * string list) list)  context =
   monad' {
+    let! tasks = getTasks()
     let! message = context.Update.Message;
     // let! text = message.Text;
+
+    for arg in arguments do
+      printf "%s: [" (fst arg)
+      for value in (snd arg) do
+        printf " %s" value
+      printfn "]"
 
     // TODO lenses
     tasks
@@ -65,28 +90,55 @@ let onTaskList (tasks: Task list) context =
   }
   |> ignore
 
+let groups (m: Match) =
+  [|for group in m.Groups -> group|]
 
-let onUpdate (tasks: Task list) (context: UpdateContext) =
+let success (group: Group) =
+  group.Success
+
+let captureGetValue (c: Capture) =
+  c.Value
+
+let tee f a =
+  f a
+  a
+
+let group (group: Group) : string * string list =
+  (group.Name, [for c in group.Captures -> c.Value])
+
+let regexCmd (pattern: Regex) (handler: (string * string list) list -> UpdateContext -> unit) (context: UpdateContext) =
+  context.Update.Message
+  |> Option.bind (fun message -> getTextForCommand context.Me message.Text)
+  // |> tee (Option.iter (printfn "Message: %s"))
+  |> Option.map (fun cmd -> pattern.Matches cmd)
+  // |> tee (Option.iter (fun matches -> for m in matches do for g in m.Groups do for c in g.Captures do printfn "notice me! %s: %s" g.Name c.Value))
+  // |> Option.map (Array.ofSeq)
+  // |> Option.map (Array.collect groups)
+  // |> Option.map (Array.filter success)
+  // |> Option.map (Array.map group)
+  // |> Option.map (Array.groupBy fst)
+  // |> Option.map (Array.map (fun t -> (fst t, snd t |> Array.map snd |> Array.collect Array.ofList |> List.ofArray )))
+  // |> Option.map (List.ofArray)
+  |> Option.map (fun matches -> [for g in (matches |> Seq.collect (fun m -> m.Groups)) -> (g.Name, [for c in g.Captures -> c.Value])])
+  |> Option.map (fun m -> handler m context)
+  |> Option.isSome
+  |> not
+
+let onUpdate (context: UpdateContext) =
   processCommands context [
     cmd "/start" onStart
     cmd "/help" onHelp
-    cmd "/list" (onTaskList tasks)
+    // regexCmd (Regex("/list (?<tags>\+[a-z]*) (?<>)", RegexOptions.Compiled)) (onTaskList)
+    regexCmd (Regex("/list( ?(?<tags>\\+[a-z]*) ?)+( ?due:(?<due>[0-9][0-9]))", RegexOptions.Compiled)) (onTaskList)
+    cmd "/list" (onTaskList [])
   ]
   |> ignore
 
 
 [<EntryPoint>]
 let main argv =
-  let mutable tasks : Task list = [];
-  Process.run
-    "task"
-    ["export"]
-    [(fun _ args ->
-        printfn "%s" args.Data
-        if (not << String.IsNullOrEmpty) args.Data then Decode.fromString Task.Decoder args.Data |> (function | Ok(task) -> tasks <- task::tasks | Error(e) -> printf "%s" e))]
-    (fun ex -> printfn "%s" ex.Message)
-
-  startBot { defaultConfig with Token = "763014420:AAG2BE6MOQR5T6l3n5gwutDOfEZmyGCHUIw" } (onUpdate tasks) None
+  printfn "Starting bot"
+  startBot { defaultConfig with Token = "763014420:AAG2BE6MOQR5T6l3n5gwutDOfEZmyGCHUIw" } (onUpdate) None
   |> Async.RunSynchronously
   |> ignore
 
